e583bfbf10aa9456f48abcd488732fd3833eda43
thienmanhvo <benficacronaldo@gmail.com>
2019-11-27 22:48:47 +0700

    home activity

---
+++ app/src/main/java/fpt/edu/cocshop/Activity/HomeActivity.java
@@ -0,0 +1,19 @@
+package fpt.edu.cocshop.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import fpt.edu.cocshop.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
